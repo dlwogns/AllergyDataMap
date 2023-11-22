0c3ceacd1f2ef912574f28f52bcc2c10e5c6ae6d
leejaehun <ldyjka@gmail.com>
2023-11-22 11:39:59 +0900

    refactor: 파일 경로 변경

--- Server/server/src/main/java/MapServer/server/LocationExcelParser/domain/LocationData.java
+++ Server/server/src/main/java/MapServer/server/LocationExcelParser/domain/LocationData.java
@@ -1,4 +1,4 @@
-package MapServer.LocationExcelParser.domain;
+package MapServer.server.LocationExcelParser.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
--- Server/server/src/main/java/MapServer/server/LocationExcelParser/repository/LocationDataRepository.java
+++ Server/server/src/main/java/MapServer/server/LocationExcelParser/repository/LocationDataRepository.java
@@ -1,6 +1,6 @@
-package MapServer.LocationExcelParser.repository;
+package MapServer.server.LocationExcelParser.repository;
 
-import MapServer.LocationExcelParser.domain.LocationData;
+import MapServer.server.LocationExcelParser.domain.LocationData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LocationDataRepository extends JpaRepository<LocationData, Long> {
