0323dc924f2f42672fb9c71565702a69a50155c3
leejaehun <ldyjka@gmail.com>
2023-12-03 06:20:27 +0900

    fix: Restemplate Bean인식 오류 해결을 위한 Bean 생성

--- Server/server/src/main/java/MapServer/server/ServerApplication.java
+++ Server/server/src/main/java/MapServer/server/ServerApplication.java
@@ -2,6 +2,8 @@ package MapServer.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -10,4 +12,8 @@ public class ServerApplication {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 }
