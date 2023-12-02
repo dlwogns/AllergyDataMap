41cf3c2b571cf80b2005406ae5150e93d312d16f
leejaehun <ldyjka@gmail.com>
2023-12-03 06:19:45 +0900

    feat(FineDustService.java): 데이터베이스 저장을 위한 Service class 생성.

---
+++ Server/server/src/main/java/MapServer/server/FineDust/service/FineDustService.java
@@ -0,0 +1,23 @@
+package MapServer.server.FineDust.service;
+
+import MapServer.server.FineDust.domain.FineDustData;
+import MapServer.server.FineDust.repository.FineDustRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FineDustService {
+    private final FineDustRepository fineDustRepository;
+
+    @Autowired
+    public FineDustService(FineDustRepository fineDustRepository){
+        this.fineDustRepository = fineDustRepository;
+    }
+    public void saveFineDustData(String sidoName, String cityName, int pm10value){
+        FineDustData fineDustData = new FineDustData();
+        fineDustData.setSidoName(sidoName);
+        fineDustData.setCityName(cityName);
+        fineDustData.setPm10value(pm10value);
+        fineDustRepository.save(fineDustData);
+    }
+}
