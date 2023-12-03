04e404358748555978e8789894bf0991903f0daf
leejaehun <ldyjka@gmail.com>
2023-12-04 03:31:05 +0900

    fix(FineDustData.java): JPArepository가 값을 제대로 읽어오지 못하는 오류 수정 : insert @Getter

--- Server/server/src/main/java/MapServer/server/FineDust/domain/FineDustData.java
+++ Server/server/src/main/java/MapServer/server/FineDust/domain/FineDustData.java
@@ -2,14 +2,14 @@ package MapServer.server.FineDust.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Setter
+@Getter
 @Table(name = "finedust")
 public class FineDustData {
 
