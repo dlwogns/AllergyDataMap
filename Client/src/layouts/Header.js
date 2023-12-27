e34608da4faf52f9e43f871d79ce321f11ec0c06
tjdvyzl <tjdvyzl12@naver.com>
2023-12-28 08:26:41 +0900

    searchPage

--- Client/src/layouts/Header.js
+++ Client/src/layouts/Header.js
@@ -1,31 +1,69 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Navbar,
+  Collapse,
+  Nav,
+  NavItem,
+  NavbarBrand,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Dropdown,
+  Button,
+} from "reactstrap";
+import { ReactComponent as LogoWhite } from "../assets/images/logos/amplelogowhite.svg";
 import user1 from "../assets/images/users/user1.jpg";
 
-function Header() {
+const Header = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [dropdownOpen, setDropdownOpen] = React.useState(false);
+
+  const toggle = () => setDropdownOpen((prevState) => !prevState);
+  const Handletoggle = () => {
+    setIsOpen(!isOpen);
+  };
+  const showMobilemenu = () => {
+    document.getElementById("sidebarArea").classList.toggle("showSidebar");
+  };
   return (
-    <Navbar expand="lg" style={{ backgroundColor: "#A7D397" }}>
-      <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-          <Nav className="mr-auto">
-            <Nav.Link href="/#login" style={{ paddingRight: "20px" }}>
+    <Navbar color="dark" dark expand="md">
+      <div className="d-flex align-items-center">
+        <Button
+          color="dark"
+          className="d-lg-none"
+          onClick={() => showMobilemenu()}
+        >
+          <i className="bi bi-list"></i>
+        </Button>
+      </div>
+
+      <Collapse navbar>
+        <Nav className="justify-content-end" navbar>
+          <NavItem>
+            <Link to="/login" className="nav-link">
               Login
-            </Nav.Link>
+            </Link>
+          </NavItem>
+        </Nav>
+        <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+          <DropdownToggle color="dark">
             <img
               src={user1}
               alt="profile"
               className="rounded-circle"
               width="30"
             ></img>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem header>Profile</DropdownItem>
+            <DropdownItem>MyPage</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </Collapse>
     </Navbar>
   );
-}
+};
 
 export default Header;
--- Client/src/pages/SearchPage.js
+++ Client/src/pages/SearchPage.js
@@ -1,5 +1,91 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import koreaRegion from "../koreaRegion";
+import Card from "react-bootstrap/Card";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import Form from "react-bootstrap/Form";
+import "../styles/searchPage.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function SearchPage() {
-  return <div>SearchPage</div>;
+  const [selectedRegion, setSelectedRegion] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+
+  useEffect(() => {
+    for (let i = 0; i < koreaRegion.length; i++) {
+      console.log(koreaRegion[i][0]);
+      for (let j = 0; j < koreaRegion[i].length; j++) {
+        console.log(koreaRegion[i][1][j]);
+      }
+    }
+  }, []);
+
+  return (
+    <div className="searchContainer">
+      <Card
+        border="light"
+        bg="dark"
+        text="white"
+        className="searchCard"
+        style={{ width: "35rem", height: "20rem" }}
+      >
+        <Card.Header>Search</Card.Header>
+        <Card.Body>
+          <InputGroup className="mb-3">
+            <DropdownButton as={ButtonGroup} size="md" title="시/도">
+              {koreaRegion.map((region, idx) => (
+                <Dropdown.Item
+                  key={idx}
+                  onClick={() => {
+                    setSelectedRegion(region[0]);
+                  }}
+                >
+                  {region[0]}
+                </Dropdown.Item>
+              ))}
+            </DropdownButton>
+            <InputGroup.Text
+              id="basic-addon3"
+              style={{ marginRight: "50px", minWidth: "150px" }}
+            >
+              {selectedRegion}
+            </InputGroup.Text>
+            {/* margin */}
+            <DropdownButton as={ButtonGroup} size="md" title="시/군/구">
+              {selectedRegion &&
+                koreaRegion
+                  .find((region) => region[0] === selectedRegion)[1]
+                  .map((city, idx) => (
+                    <Dropdown.Item
+                      key={idx}
+                      onClick={() => {
+                        setSelectedCity(city);
+                      }}
+                    >
+                      {city}
+                    </Dropdown.Item>
+                  ))}
+            </DropdownButton>
+            <InputGroup.Text id="basic-addon3" style={{ minWidth: "150px" }}>
+              {selectedCity}
+            </InputGroup.Text>
+          </InputGroup>
+          <Button
+            variant="outline-success"
+            style={{
+              margin: "70px",
+              marginLeft: "180px",
+              width: "10em",
+              height: "3em",
+            }}
+          >
+            Search
+          </Button>{" "}
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
